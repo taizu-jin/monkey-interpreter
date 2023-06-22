34219a471c9d9e35eca3c6216fcbcf38ee401477
taizu-jin <mantas.scc@gmail.com>
2023-06-22 16:07:00 +0300

    feat: add evaluation to Read-Evaluate-Print-Loop(REPL)

--- repl/repl.go
+++ repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"monkey-interpreter/evaluator"
 	"monkey-interpreter/lexer"
 	"monkey-interpreter/parser"
 )
@@ -27,10 +28,14 @@ func Start(in io.Reader, out io.Writer) {
 		program := p.ParseProgram()
 		if len(p.Errors()) != 0 {
 			printParserErrors(out, p.Errors())
+			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 

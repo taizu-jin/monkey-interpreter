37990ba294a3e1cca80748047f252de01358b093
taizu-jin <mantas.scc@gmail.com>
2023-06-27 12:17:01 +0300

    test: parse string literals

--- ast/ast.go
+++ ast/ast.go
@@ -21,6 +21,15 @@ type Expression interface {
 	expressionNode()
 }
 
+type StringLiteral struct {
+	Token token.Token
+	Value string
+}
+
+func (sl *StringLiteral) expressionNode()      {}
+func (sl *StringLiteral) TokenLiteral() string { return sl.Token.Literal }
+func (sl *StringLiteral) String() string       { return sl.Token.Literal }
+
 type CallExpression struct {
 	Token     token.Token // The '(' token
 	Function  Expression  // Identifier or FunctionLiteral

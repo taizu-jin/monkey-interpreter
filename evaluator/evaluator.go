7ed42f6f344a2865f98391b0d187ea26faf65b31
taizu-jin <mantas.scc@gmail.com>
2023-06-27 12:34:46 +0300

    feat: evaluate string concatination

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -193,6 +193,8 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 	case operator == "==":
 		return nativeBoolToBoolObject(left == right)
 	case operator == "!=":
@@ -202,6 +204,17 @@ func evalInfixExpression(operator string, left object.Object, right object.Objec
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(operator string, left, right object.Object) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+	return &object.String{Value: leftVal + rightVal}
+
 }
 
 func evalIntegerInfixExpression(operator string, left, right object.Object) object.Object {

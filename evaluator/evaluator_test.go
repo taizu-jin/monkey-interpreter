ab43f23a3b984fede0d7bc1259f279a165c43f06
taizu-jin <mantas.scc@gmail.com>
2023-06-27 18:20:50 +0300

    test(builtin): evaluate first function

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -361,6 +361,9 @@ func TestBuiltinFunction(t *testing.T) {
 		{`len("one", "two")`, "wrong number of arguments. got=2, want=1"},
 		{`len([1, 2, 3])`, 3},
 		{`len([])`, 0},
+		{`first([1, 2, 3])`, 1},
+		{`first([])`, nil},
+		{`first(1)`, "argument to `first` must be ARRAY, got INTEGER"},
 	}
 
 	for _, tt := range tests {
@@ -369,6 +372,8 @@ func TestBuiltinFunction(t *testing.T) {
 		switch expected := tt.expected.(type) {
 		case int:
 			testIntegerObject(t, evaluated, int64(expected))
+		case nil:
+			testNullObject(t, evaluated)
 		case string:
 			errObj, ok := evaluated.(*object.Error)
 			if !ok {

2e52e4933fb8807ce94cab2624672002f0a95f7a
taizu-jin <mantas.scc@gmail.com>
2023-06-27 15:59:07 +0300

    feat: tokenize array

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -69,6 +69,10 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LT, l.ch)
 	case '>':
 		tok = newToken(token.GT, l.ch)
+	case '[':
+		tok = newToken(token.LBRACKET, l.ch)
+	case ']':
+		tok = newToken(token.RBRACKET, l.ch)
 	case '{':
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
